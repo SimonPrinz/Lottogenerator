d34c208cb1d002d2bc6f856de1efc507fff1f333
Simon Prinz <simon.prinz@live.de>
2019-04-23 13:52:23 +0200

    Initial Commit

---
+++ Lottogenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lottogenerator
+//
+//  Created by Simon Prinz on 23.04.19.
+//  Copyright © 2019 Simon Peinz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
